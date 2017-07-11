da3bf3de7aabd4d8ca4c4c5fdbfa5bd8953ddd9a
Creighton <creighton.harrington@gmail.com>
2017-07-11 13:06:21 -0400

    Pushing

--- Creighton/paircount.cpp
+++ Creighton/paircount.cpp
@@ -1,4 +1,3 @@
-//128516
 #include <unordered_set>
 #include <fstream>
 #include <vector>
@@ -9,9 +8,25 @@
 #include <pthread.h>
 #include <mutex>
 #include <time.h>
+#include <unordered_map>
 
 //mutual exclusion lock
 std::mutex mtx;
+
+//combination formula
+double nChoose2(unsigned n) {
+    
+    if (2 > n)
+    	return 0;
+
+    double result = n;
+    for(int i = 2; i <= 2; ++i) {
+        result *= (n - i + 1);
+        result /= i;
+    }
+
+    return result;
+} //end combination formula
 
 //struct containing thread data
 struct thread_data {
@@ -20,7 +35,7 @@ struct thread_data {
 
 	std::vector<std::unordered_set<std::string>>* docs;
 	std::vector<std::vector<std::string>>* top20s;
-	std::vector<int>* paircounts;
+	std::unordered_map<std::string, int>* pairmap;
 
 	int limits[2];
 
@@ -35,7 +50,8 @@ void* checkpairs(void* args) {
 	//this just makes the loop below less codey
 	std::vector<std::unordered_set<std::string>>* docs = data->docs;
 	std::vector<std::vector<std::string>>* top20s = data->top20s;
-	std::vector<int>* paircounts = data->paircounts;
+	std::unordered_map<std::string, int>* pairmap = data->pairmap;
+
 	int left = data->limits[0];
 	int right = data->limits[1];
 
@@ -54,12 +70,16 @@ void* checkpairs(void* args) {
 
 						//critical section: iterate counts
 						mtx.lock();
-						paircounts->at(b)++;
+						try {
+							pairmap->at( top20s->at(b)[i] + "-" + top20s->at(b)[j] )++;
+						}
+						catch(const std::out_of_range& e) {
+							pairmap->insert({ top20s->at(b)[i] + "-" + top20s->at(b)[j], 1 });
+						}
 						mtx.unlock();
 					
 					} //end if
 				} //end for
-
 			} //end for
 		} //end for
 
@@ -71,172 +91,237 @@ void* checkpairs(void* args) {
 } //end paircounts
 
 //main program
-int main() {
+int main(int argc, char* argv[]) {
 
-	//number of threads (for later)
-	size_t NUM_THREADS = 4;
+	std::vector<int> dims = { 50, 150, 200, 250, 300, 350, 400, 450, 550 };
 
-	//how many docs to check per go
-	//picked a number which will leave no
-	//remainder lines in main loop
-	size_t basket = 979;
+	time_t start, stop;
+	start = time(0);
 
-	//Top 20s
-	std::fstream top20("Pre_Paircounting/Top20_600d.txt", std::ios::in);
+	for(int d = 0; d < dims.size(); d++) {
 
-	//top 20s
-	std::vector<std::vector<std::string>> top20s(100);
+		//which dimension are we on right now?
+		std::string dim = std::to_string(dims[d]);
 
-	//load the wordbags into memory
-	size_t i = 0;
-	std::string line;
-	size_t outct = 1;
-	while(std::getline(top20, line)) {
+		//number of threads (for later)
+		size_t NUM_THREADS = 4;
 
-		if(line == "###")
-			i++;
+		//how many docs to check per go
+		//picked a number which will leave no
+		//remainder lines in main loop
+		size_t basket = 979;
 
-		//add to the temp vector
-		if(line != "###")
-			top20s[i].push_back(line);
+		//Word counts
+		std::fstream wordcounts("../../Misc/vocabcount.txt", std::ios::in);
+		std::unordered_map<std::string, int> countmap;
 
-	} //end while
+		//word pair map for each pairs gross count
+		std::unordered_map<std::string, int> pairmap;
 
-	top20.close();
+		//holder string for all of our loops
+		std::string line;
 
-	//Cleaned Reviews
-	std::fstream cleaned("cleaned_reviews.txt", std::ios::in);
+		//make hashmap of the word and its doccount
+		while(std::getline(wordcounts, line)) {
 
-	//list of lines, each lne split into tokens
-	std::vector<std::unordered_set<std::string>> docs(basket);
-
-	//actual paircounts of each cluster
-	std::vector<int> paircounts(100, 0);
-
-	//times
-	time_t t1, t2;
-	t1 = time(0);
-
-	//add all of the tokenized lines to the doc list
-	i = 0;
-	size_t ckct = 1;
-	while(std::getline(cleaned, line)) {
-
-		//holder set
-		std::unordered_set<std::string> tempset;
-
-		//put each token from the line in the temp vector
-		size_t pos = 0;
-		while(pos != std::string::npos) {
-			pos = line.find(" ");
 			std::string sub = line.substr(0, line.find(" "));
 			line = line.substr(line.find(" ") + 1, line.length());
-			tempset.insert(sub);
+			countmap[sub] = atof(line.c_str());
+
 		} //end while
 
-		//add the tokenized line to the master doc list
-		docs[i++] = tempset;
+		wordcounts.close();
 
-		//we only want to a section at a time to save memory
-		if(i == basket) {
+		//Top 20s
+		std::fstream top20("../../Pre_Paircounting/Top20_" + dim + "d.txt", std::ios::in);
 
-			//our loading bar
-			std::cout << "\r" << ((float)ckct++ / (float)(2827352 / basket)) * 100 << "\% complete.";
-			std::cout.flush();
+		//top 20s
+		std::vector<std::vector<std::string>> top20s(100);
 
-			//reset the i counter
-			i = 0;
+		//load the wordbags into memory
+		size_t i = 0;
+		while(std::getline(top20, line)) {
 
-			//the amount of lines each thread will check
-			int width = basket / NUM_THREADS;
+			if(line == "###")
+				i++;
 
-			//threads setup
-			pthread_t threads[NUM_THREADS];
-			pthread_attr_t attr;
-			pthread_attr_init(&attr);
-			pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-			void* status;
-			int rc;
-			struct thread_data* td[NUM_THREADS];
-			for(int j = 0; j < NUM_THREADS; j++)
-				td[j] = new thread_data();
+			//add to the temp vector
+			if(line != "###")
+				top20s[i].push_back(line);
 
-			//loop drives the thread calls
-			for(int j = 0; j < NUM_THREADS; j++) {
+		} //end while
 
-				//set thread ids
-				td[j]->thread_id = j;
+		top20.close();
 
-				//pointer to top20s for each thread (reading only, so no threat of race condition)
-				td[j]->top20s = &top20s;
+		//Cleaned Reviews
+		std::fstream cleaned("../../cleaned_reviews.txt", std::ios::in);
 
-				//pointer to the list of doc set (reading only, so no threat of race condition)
-				td[j]->docs = &docs;
+		//list of lines, each lne split into tokens
+		std::vector<std::unordered_set<std::string>> docs(basket);
 
-				//pointer to pairwise counts (writing, so mutex will be used in function above)
-				td[j]->paircounts = &paircounts;
+		//times
+		time_t t1, t2, t3;
+		t1 = time(0);
 
-				//split the amount of file to b checked by each thread
-				td[j]->limits[0] = (j * width);
-				if(j == NUM_THREADS - 1)
-					td[j]->limits[1] = basket;
-				else
-					td[j]->limits[1] = ((j + 1) * width);
+		//add all of the tokenized lines to the doc list
+		i = 0;
+		size_t ckct = 0;
+		while(std::getline(cleaned, line)) {
 
-				//spawn thread
-				rc = pthread_create(&threads[j], &attr, checkpairs, (void*)(td[j]));
+			//holder set
+			std::unordered_set<std::string> tempset;
 
-				//in case thread creation didn't work
-				if(rc) {
-					std::cout << "Error: Unable to create thread " << rc << std::endl;
-					exit(-1);
-				} //end if
+			//put each token from the line in the temp vector
+			size_t pos = 0;
+			while(pos != std::string::npos) {
+				pos = line.find(" ");
+				std::string sub = line.substr(0, line.find(" "));
+				line = line.substr(line.find(" ") + 1, line.length());
+				tempset.insert(sub);
+			} //end while
 
-			} //end for
+			//add the tokenized line to the master doc list
+			docs[i++] = tempset;
 
-			//reset the attr
-			pthread_attr_destroy(&attr);
+			//we only want to a section at a time to save memory
+			if(i == basket) {
 
-			//wait for the threads to finish before continuing
-			for(int j = 0; j < NUM_THREADS; j++) {
+				//our loading bar
+				std::cout << "\r" << (int)(((float)ckct++ / (float)(2827352 / basket)) * 100) << "\% complete";
+				std::cout.flush();
 
-				//wait for thread j to finish
-				rc = pthread_join(threads[j], &status);
+				//reset the i counter
+				i = 0;
 
-				//error joining thread
-				if(rc) {
-					std::cout << "Error: unable to join " << rc << std::endl;
-					exit(-1);
-				} //end if
+				//the amount of lines each thread will check
+				int width = basket / NUM_THREADS;
 
-			} //end for
+				//threads setup
+				pthread_t threads[NUM_THREADS];
+				pthread_attr_t attr;
+				pthread_attr_init(&attr);
+				pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+				void* status;
+				int rc;
+				struct thread_data* td[NUM_THREADS];
+				for(int j = 0; j < NUM_THREADS; j++)
+					td[j] = new thread_data();
 
-			//delete heap data
-			for(int j = 0; j < NUM_THREADS; j++)
-				delete td[j];
+				//loop drives the thread calls
+				for(int j = 0; j < NUM_THREADS; j++) {
 
-		} //end if
+					//set thread ids
+					td[j]->thread_id = j;
 
+					//pointer to top20s for each thread (reading only, so no threat of race condition)
+					td[j]->top20s = &top20s;
 
-	} //end while
+					//pointer to the list of doc set (reading only, so no threat of race condition)
+					td[j]->docs = &docs;
 
-	//display how long everything took
-	t2 = time(0);
-	std::cout << std::endl << difftime(t2, t1) / 60 / 60 << std::endl;
+					//pointer to the pair map
+					td[j]->pairmap = &pairmap;
+
+					//split the amount of file to b checked by each thread
+					td[j]->limits[0] = (j * width);
+					if(j == NUM_THREADS - 1)
+						td[j]->limits[1] = basket;
+					else
+						td[j]->limits[1] = ((j + 1) * width);
+
+					//spawn thread
+					rc = pthread_create(&threads[j], &attr, checkpairs, (void*)(td[j]));
+
+					//in case thread creation didn't work
+					if(rc) {
+						std::cout << "Error: Unable to create thread " << rc << std::endl;
+						exit(-1);
+					} //end if
+
+				} //end for
+
+				//reset the attr
+				pthread_attr_destroy(&attr);
+
+				//wait for the threads to finish before continuing
+				for(int j = 0; j < NUM_THREADS; j++) {
+
+					//wait for thread j to finish
+					rc = pthread_join(threads[j], &status);
+
+					//error joining thread
+					if(rc) {
+						std::cout << "Error: unable to join " << rc << std::endl;
+						exit(-1);
+					} //end if
+
+				} //end for
+
+				//delete heap data
+				for(int j = 0; j < NUM_THREADS; j++)
+					delete td[j];
+
+			} //end if
+
+		} //end while
+
+		//close the file
+		cleaned.close();
+
+		//now that everything is counted, lets find the probabilities
+
+		//average probabilities
+		std::vector<double> probs(100, 0);
+		
+		//go through each cluster
+		for(i = 0; i < 100; i++) {
+
+			//got through each pair
+			for(int j = 0; j < top20s[i].size(); j++) {
+				for(int k = j + 1; k < top20s[i].size(); k++) {
+
+					std::string lw = top20s[i][j];
+					std::string rw = top20s[i][k];
+					double pc = (double)pairmap[lw + "-" + rw];
+					
+					//average of p(lw+rw|lw) and p(lw+rw|rw)
+					probs[i] += ( pc / (double)countmap[lw] + pc / (double)countmap[rw] ) / 2;
+
+				}
+			}
+
+			//find average of all the probabilities in the cluster
+			probs[i] = probs[i] / nChoose2(top20s[i].size());
+
+		}
+
+		t2 = time(0);
+		double hrs = difftime(t2, t1) / 60 / 60;
+		int rh = (int)hrs;
+		int rm = (int)((hrs - rh) * 60);
+		int rs = (int)( (((hrs - rh) * 60) - (int)((hrs - rh) * 60)) * 60 );
+		std::cout << std::endl << "Total runtime: " << rh << " hour(s), " << rm << " minutes, and " << rs << " seconds." << std::endl;
+		std::cout.flush();
+
+		//output results
+		std::fstream results("../../Cluster_Metrics/pairprobs_" + dim + "d.txt", std::ios::out);
+
+		//write the results file
+		for(auto i : probs)
+			results << i << std::endl;
+
+		//stitch em up, we're done here
+		results.close();
+
+	}
+
+	stop = time(0);
+	double hrs = difftime(stop, start) / 60 / 60;
+	int rh = (int)hrs;
+	int rm = (int)((hrs - rh) * 60);
+	int rs = (int)( (((hrs - rh) * 60) - (int)((hrs - rh) * 60)) * 60 );
+	std::cout << std::endl << "TOTAL Total runtime: " << rh << " hour(s), " << rm << " minutes, and " << rs << " seconds." << std::endl;
 	std::cout.flush();
-
-	//close the file
-	cleaned.close();
-
-	//output results
-	std::fstream results("Cluster_Metrics/paircounts_600d.txt", std::ios::out);
-
-	//write the results file
-	for(auto i : paircounts)
-		results << i << std::endl;
-
-	//stitch em up, we're done here
-	results.close();
 
 	return 0;
 
