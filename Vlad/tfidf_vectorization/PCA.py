ab80ddeeba634b47e5bd968b74ab4d7376178549
UladKasach <nappa300@gmail.com>
2017-08-09 00:12:23 -0400

    Add TF-IDF vectorization, being PCA which may be desired on results

---
+++ Vlad/tfidf_vectorization/PCA.py
@@ -0,0 +1,36 @@
+import numpy as np
+from sklearn import decomposition
+
+'''
+
+input_file_path = "results/vectors.txt";
+output_file_path = "results/vectors_PCA.txt";
+
+
+np.random.seed(5)
+
+X = iris.data
+y = iris.target
+
+
+plt.cla()
+pca = decomposition.PCA(n_components=3)
+pca.fit(X)
+X = pca.transform(X)
+
+for name, label in [('Setosa', 0), ('Versicolour', 1), ('Virginica', 2)]:
+    ax.text3D(X[y == label, 0].mean(),
+              X[y == label, 1].mean() + 1.5,
+              X[y == label, 2].mean(), name,
+              horizontalalignment='center',
+              bbox=dict(alpha=.5, edgecolor='w', facecolor='w'))
+# Reorder the labels to have colors matching the cluster results
+y = np.choose(y, [1, 2, 0]).astype(np.float)
+ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=y, cmap=plt.cm.spectral)
+
+ax.w_xaxis.set_ticklabels([])
+ax.w_yaxis.set_ticklabels([])
+ax.w_zaxis.set_ticklabels([])
+
+plt.show()
+'''
